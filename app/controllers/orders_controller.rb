b3f6633ef6abdef463b88f3d6ee5a3f71f59715e
Francisco Rojas <josefcorojas@gmail.com>
2015-06-16 23:00:47 -0600

    Add delete link to items in checkout view

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,9 @@
 class OrdersController < ApplicationController
   def checkout
   end
+
+  def remove_item
+    PurchaseService.new(current_order).remove_from_cart(params[:id])
+    redirect_to checkout_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
 
   resources :items
   get '/checkout', to: "orders#checkout"
+  delete '/remove_item/:id', to: "orders#remove_item", as: :remove_item
 end
